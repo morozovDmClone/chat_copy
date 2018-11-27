cdd9c3e637ff4c76c463499954bf0870560ea71f
morozovDmClone <44333243+morozovDmClone@users.noreply.github.com>
2018-11-27 10:10:13 +0400

    test_2

---
+++ src/components/Chatroom/Chatroom.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './Chatroom.css'
+import Message from '../Message/Message.js'
+
+class Chatroom extends Component {
+ 
+  componentDidMount() {
+    ReactDOM.findDOMNode(this.refs.messagesList).scrollTop = ReactDOM.findDOMNode(this.refs.messagesList).scrollHeight
+  }
+
+  componentDidUpdate() {
+    ReactDOM.findDOMNode(this.refs.messagesList).scrollTop = ReactDOM.findDOMNode(this.refs.messagesList).scrollHeight
+  }
+
+  render() {
+    let { messages } = this.props;
+    return (
+      <div className="Chatroom">
+        <div className='MessagesList-container' ref='messagesList'>
+          <ul className='messagesList' >
+            {
+              messages.map(message =>
+                <Message key={message.id} message={message} user={this.props.username} />
+              )
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Chatroom;
